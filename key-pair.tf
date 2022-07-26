f7c1959b03732263987ae757f03ffb4e216b74b7
lutilipe <lutilipe02@hotmail.com>
2022-07-26 14:21:38 -0300

    refactor: remove ec2 from rpovider and add key pair

---
+++ key-pair.tf
@@ -0,0 +1,3 @@
+data "aws_key_pair" "dev_luiz" {
+  key_name = "dev-luiz"
+}
--- provider.tf
+++ provider.tf
@@ -12,12 +12,3 @@ terraform {
 provider "aws" {
     region = var.provider_region
 }
-
-resource "aws_instance" "mc_server" {
-    ami           = var.instance_ami
-    instance_type = var.instance_type
-
-    tags = {
-        Name = var.instance_name
-    }
-}
